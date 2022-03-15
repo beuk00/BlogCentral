9cd35566037a87b9f830d376ca623f192b06578e
Kamiel Rooselaer <92980077+Kam13l@users.noreply.github.com>
2022-03-15 15:04:21 +0100

    [UPD] add comment to model

--- BlogCentralApp/Models/DetailIndexViewModel.cs
+++ BlogCentralApp/Models/DetailIndexViewModel.cs
@@ -9,5 +9,6 @@ namespace BlogCentralApp.Models
     public class DetailIndexViewModel
     {
         public BlogPost blogPost;
+        public bool hasLiked;
     }
 }
---
+++ BlogCentralLib/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlogCentralLib.Entities
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int BlogpostId { get; set; }
+        public string AuthorId { get; set; }
+        public string Content { get; set; }
+    }
+}
