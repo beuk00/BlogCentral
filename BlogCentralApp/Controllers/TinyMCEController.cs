e772964ba18aa5d6ea814a4b5bb9c75757abd6f3
Poniatowski Maximilian <92987402+maksOnGit@users.noreply.github.com>
2022-03-24 02:33:04 +0100

    [UPD] First attempt

---
+++ BlogCentralApp/Controllers/TinyMCEController.cs
@@ -0,0 +1,20 @@
+﻿using BlogCentralApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCentralApp.Controllers
+{
+    public class TinyMCEController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(TinyTest model)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ BlogCentralApp/Models/TinyTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace BlogCentralApp.Models
+{
+    public class TinyTest
+    {
+
+            // This attributes allows your HTML Content to be sent up  
+            [AllowHtml]
+            public string HtmlContent { get; set; }
+
+            public TinyTest()
+            {
+
+            }
+
+    }
+}
