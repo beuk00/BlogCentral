1c194336a39f2ac8828033bdde6cedd1056cd602
Poniatowski Maximilian <92987402+maksOnGit@users.noreply.github.com>
2022-03-29 01:54:52 +0200

    [UPD] Working tinyMCE Editor

--- BlogCentralApp/wwwroot/js/site.js
+++ BlogCentralApp/wwwroot/js/site.js
@@ -8,17 +8,20 @@
     tooltipTriggerList.forEach(function (tooltipTriggerEl) {
         new bootstrap.Tooltip(tooltipTriggerEl)
     })
-})()
+})();
 
+
+//Implementing RichTextEditor with tinyMCE. 
 tinymce.init({
     selector: '#editable',
-    plugins: ['autoresize', 'autolink'],
+    plugins: ['autoresize', 'autolink', 'image', 'advlist', 'preview'],
     autoresize_bottom_margin: 0,
+    statusbar: false,
     brand: false,
     menubar: false,
     default_link_target: '_blank',
     link_default_protocol: 'https',
-    statusbar: false,
-
-
+    min_height: 400,
+    toolbar: 'undo redo | lineheight  forecolor | bold italic underline strikethrough | alignleft aligncenter alignright | image | selectall preview|',
+ 
 });
