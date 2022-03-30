99cb2c7def9065e0a024b504aa463f966b98b821
Quinten De Clerck <quintendc@users.noreply.github.com>
2022-03-30 11:17:57 +0200

    [WIP]

---
+++ BlogCentralApp/Hubs/VisitorsHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace BlogCentralApp.Hubs
+{
+    public class VisitorsHub : Hub
+    {
+        public async Task Visit(string user, string message)
+        {
+            await Clients.All.SendAsync("Visit", "visit send async");
+        }
+    }
+}
